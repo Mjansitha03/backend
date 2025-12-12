4d49e334e87a3d16c64f075178f899bc26635f11
Mjansitha03 <mjansitha03@gmail.com>
2025-12-12 15:08:54 +0530

    link updated

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -105,7 +105,7 @@ export const forgotPassword = async (req, res) => {
 
     // *** LOCALHOST REACT FRONTEND ***
     // frontend URL for reset link
-    const frontendURL = "https://frontend-tau-dun-36.vercel.app";
+    const frontendURL = "https://frontend-eweh.vercel.app";
     const url = `${frontendURL}/reset-password/${user._id}/${resetToken}`;
 
     // send email with link
