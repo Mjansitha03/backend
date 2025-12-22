441f20a8c9f799916014cdddbca01d20b5fe946b
Mjansitha03 <mjansitha03@gmail.com>
2025-12-22 12:14:31 +0530

    updated

--- Utils/mailer.js
+++ Utils/mailer.js
@@ -3,35 +3,57 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// export const sendmail = async (to, subject, text) => {
+//   const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     host: 'smtp.gmail.com',
+//     tls: {
+//       rejectUnauthorized: false
+//     },
+//     port: 465,
+//     secure: false,
+//     auth: {
+//       user: process.env.FROM_EMAIL,
+//       pass: process.env.PASS_KEY
+//     }
+//   });
+
+//   const mailData = {
+//     from: process.env.FROM_EMAIL,
+//     to,
+//     subject,
+//     text,
+//   };
+//   await new Promise((resolve, reject) => {
+//     transporter.sendMail(mailData, (err, info) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         console.log("Mail Send Successfully")
+//         resolve(info);
+//       }
+//     });
+//   });
+// };
+
+
 export const sendmail = async (to, subject, text) => {
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    host: 'smtp.gmail.com',
-    tls: {
-      rejectUnauthorized: false
-    },
-    port: 465,
+    host: "smtp.gmail.com",
+    port: 587,
     secure: false,
     auth: {
       user: process.env.FROM_EMAIL,
-      pass: process.env.PASS_KEY
-    }
+      pass: process.env.PASS_KEY,
+    },
   });
 
-  const mailData = {
+  await transporter.sendMail({
     from: process.env.FROM_EMAIL,
     to,
     subject,
     text,
-  };
-  await new Promise((resolve, reject) => {
-    transporter.sendMail(mailData, (err, info) => {
-      if (err) {
-        reject(err);
-      } else {
-        console.log("Mail Send Successfully")
-        resolve(info);
-      }
-    });
   });
+
+  console.log("Mail sent successfully");
 };
